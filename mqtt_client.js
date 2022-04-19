4a9b3cee69b75813ce8ad132e9511c835e2cc47f
mvvkinn <wo7910@naver.com>
2022-04-20 00:32:27 +0900

    Feature Mysql connection

--- mqtt_client.js
+++ mqtt_client.js
@@ -1,12 +1,13 @@
 var mqtt = require('mqtt');
-var mysql = require('mysql')
+var mysql = require('mysql');
 
-var mqtt_options = {
-    host: 'mvvkinn.xyz',
+// mqtt options
+var options = {
+    host: 'localhost',
     port : 1883,
     clientId : 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    username : "mjc",
-    password : "mjc1234",
+    username : "",
+    password : "",
     keepalive : 60,
     reconnectPeriod : 1000,
     encoding : "utf8"
@@ -14,17 +15,18 @@ var mqtt_options = {
 
 var client = mqtt.connect(options);
 
-// Connect and subscribe
-client.on ("connect",function(mqtt_options){
-    client.subscribe('examples/#', function(err){
+// MQTT Connect and subscribe
+client.on ("connect",function(options){
+    client.subscribe('examples', function(err){
         if (!err){
             client.publish('presense', 'hello mqtt');
         }
     });
-    console.log('Connected to MQTT Server');
+    console.log('Server : Connected to MQTT Server');
 
 });
 
+// Message callback
 client.on ("message", function(topic, message, packet){
     console.log("----------new message----------");
     console.log("topic : " + topic + "\nmessage : " + message.toString());
@@ -35,3 +37,21 @@ client.on ("message", function(topic, message, packet){
 client.on('error', function(err) {
     console.log(err);
 });
+
+// MySQL 
+var db_conn = mysql.createConnection({
+    host: "localhost",
+    user: "helmet_admin",
+    password: "!helmAdmin",
+    database: "testDB",
+    port: "3306"
+})
+
+db_conn.connect(function(err){
+    if (err) {
+        throw err;
+    } 
+    console.log("Server : Database connected");
+})
+
+
