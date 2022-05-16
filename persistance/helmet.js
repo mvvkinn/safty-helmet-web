422f3dae05e9e4e8a4f528a171e26fc4ee290dce
mvvkinn <wo7910@naver.com>
2022-05-16 12:16:59 +0900

    Add helmet DB CURD

--- persistance/helmet.js
+++ persistance/helmet.js
@@ -1,30 +1,26 @@
-const models = require('./models');
+const models = require("./models");
 
-let uploadData = function (data){
-    let data_placed = {'helmet_id' : data.helmet_id};
+let uploadData = function (data) {
+  let data_placed = { helmet_id: data.helmet_id };
 
-    models.helmet_test.findOne({where : data_placed}).then(
-        hasValueFound => {
-            if(hasValueFound){
-                console.log("updating helmet data");
-                models.helmet_test.update(data, {where : data_placed});
-            } else {
-                console.log("new helmet detected, creating new data");
-                models.helmet_test.create(data);
-            }
-        }
-    )
-}
+  models.helmet_test.findOne({ where: data_placed }).then(hasValueFound => {
+    if (hasValueFound) {
+      models.helmet_test.update(data, { where: data_placed });
+    } else {
+      models.helmet_test.create(data);
+    }
+  });
+};
 
-let dataFrom = function (helmet_id) {
+let readDataFromHelmetId = function (helmet_id) {
   let data_placed = { helmet_id: helmet_id };
 
-  return new Promise((resolve) => {
-    models.helmet_test.findOne({ where: data_placed }).then((datas) => {
+  return new Promise(resolve => {
+    models.helmet_test.findOne({ where: data_placed }).then(datas => {
       resolve(datas);
     });
   });
 };
 
 module.exports.uploadData = uploadData;
-module.exports.dataFrom = dataFrom;
+module.exports.readDataFromHelmetId = readDataFromHelmetId;
