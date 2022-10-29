49bdde70ecf6187e75635f4db22b1fce4bb1c2ea
mvvkinn <wo7910@naver.com>
2022-10-29 17:00:07 +0900

    Add helmet, field, worker interfaces

---
+++ src/interfaces/IField.ts
@@ -0,0 +1,5 @@
+export interface IField {
+  field_id: number;
+  field_name: string;
+  field_addr: string;
+}
---
+++ src/interfaces/IWorker.ts
@@ -0,0 +1,7 @@
+export interface IWorker {
+  worker_id: number;
+  worker_name: string;
+  worker_position: string;
+  group_name: string;
+  field_id: number;
+}
