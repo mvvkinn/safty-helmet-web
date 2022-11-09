eb98e0fadf1c0dee7f30d0157e594b4208aa94a3
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-11-09 22:30:43 +0900

    Fix app listener to httpServer with socket.io

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,20 @@ import express from "express";
 import config from "@config";
 import loaders from "@loaders";
 import logger from "@loaders/logger";
+import socketio, { Server } from "socket.io";
+import { createServer } from "http";
 
 async function startService() {
   const app = express();
+  const httpServer = createServer(app);
 
-  await loaders(app);
+  await loaders(app, httpServer);
 
-  app.listen(config.port, () => {
+  httpServer.listen(config.port, () => {
     logger.info(`Server running on http://localhost:${config.port}`);
   });
 }
+
+process.setMaxListeners(15);
 
 startService();
