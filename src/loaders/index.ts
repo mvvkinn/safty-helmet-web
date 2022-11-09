c3feb5dea197c0211c38de8d987f20eeb20e278f
Minwoo Kim <mvvkinn@mvvkinn.xyz>
2022-11-09 22:28:01 +0900

    Add socket.io, mqtt on loader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,20 +1,22 @@
 import expressLoader from "./express";
-import mqttLoader from "./mqtt";
+import mqttInstance from "./mqtt";
 import { Application } from "express";
 import logger from "./logger";
 import dependencyInjector from "./dependencyInjector";
 import sequelizeLoader from "./sequelize";
+import socketIo from "./socket.io";
+import { Server } from "http";
 
-export default async (expressApp: Application) => {
-  const sequelizeInstance = await sequelizeLoader();
+export default async (expressApp: Application, httpServer: Server) => {
+  const sequelizeInstance = await sequelizeLoader;
   logger.info("DB Connected");
 
   await expressLoader(expressApp);
   logger.info("Express Loaded");
 
-  const mqttClient = await mqttLoader();
+  const mqttClient = mqttInstance;
   logger.info("MQTT Loaded");
 
-  await dependencyInjector(sequelizeInstance);
+  await dependencyInjector(sequelizeInstance, mqttClient, httpServer);
   logger.info("Dependency Injector loaded\n");
 };
--- src/loaders/mqtt.ts
+++ src/loaders/mqtt.ts
@@ -1,38 +1,6 @@
 import mqtt from "mqtt";
 import config from "@config";
 
-export default async () => {
-  const client = await mqtt.connect(config.mqtt);
+const client = mqtt.connect(config.mqtt);
 
-  client.on("connect", options => {
-    client.subscribe("helmet", err => {
-      if (err) console.log(err);
-    });
-
-    console.log("Connected to MQTT Server");
-  });
-
-  client.on("message", (topic, msg, packet) => {
-    const json = stringToJson(String(msg));
-
-    /**
-     * @TODO Socket emit 추가 json = "helmetMsg", string = "msg"
-     */
-    if (!json) {
-    }
-  });
-};
-
-/**
- * Convert string to JSON
- * @param str
- * @returns typeof(JSON.parse(str)) == JSON ? JSON : false
- */
-function stringToJson(str: string) {
-  try {
-    const json = JSON.parse(str);
-    return json;
-  } catch (e) {
-    return false;
-  }
-}
+export default client;
